2db4d50959c187c4f663f38431e0420b41764720
Ruben Verhoef <ruben_verhoef@hotmail.com>
2019-08-08 21:14:11 +0200

    Use the FreeBSD release of Adguard

--- post_install.sh
+++ post_install.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+
+cd / && curl -s https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep "AdGuardHome_freebsd_amd64.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+tar -xvzf AdGuardHome*.tar.gz -C /root
+rm AdGuardHome*.tar.gz
+
 chmod +x /usr/local/etc/rc.d/adguard
+sysrc 'adguard_enable=YES'
+service adguard start
